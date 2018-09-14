3dbd6cb1b5f329faf945d6944bbf6a151d5fa437
Anton Danylenko <adanylenko@stuy.edu>
2018-09-14 06:19:14 -0400

    added the function that returns a randomly selected occupation

--- 03_occupation/03_occupation.py
+++ 03_occupation/03_occupation.py
@@ -1,3 +1,5 @@
+import random
+
 def main():
     file = open("occupations.csv", "r")
     #open the data file and reads the contents
@@ -47,6 +49,27 @@ def main():
         #of every line, so we must remove them.
         dict[occupation]= (float)(percent)
         #the occupation and percent of each line are logged in the dictionary
-    print(dict)
+    randNum=random.random()*dict["Total"]
+    #gets a random number with one decimal from 0 to 99.7, starts at .1 because starting at 0 would mean that if the first occ had 0%, it could be chosen
+    counter=0.0
+    for occ in dict:
+        if occ=="Total":
+            print("current counter: ",counter)
+            continue
+            # in case total comes out of order because it would always become true
+        if randNum <= counter+dict[occ]: #check if the rand number is between the original counter # and the max percent for the occ
+            return occ
+        else:
+            counter+=dict[occ]
 
-main()
+
+
+counters={"Management":0, 'Business and Financial operations': 0, 'Computer and Mathematical': 0, 'Architecture and Engineering': 0, 'Life, Physical and Social Science': 0, 'Community and Social Service': 0, 'Legal': 0, 'Education, training and library': 0, 'Arts, design, entertainment, sports and media': 0, 'Healthcare practioners and technical': 0, 'Healthcare support': 0, 'Protective service': 0, 'Food preparation and serving': 0, 'Building and grounds cleaning and maintenance': 0, 'Personal care and service': 0, 'Sales': 0, 'Office and administrative support': 0, 'Farming, fishing and forestry': 0, 'Construction and extraction': 0, 'Installation, maintenance and repair': 0, 'Production': 0, 'Transportation and material moving': 0, 'Total': 0}
+for i in range(1,1000):
+    test=main()
+    counters[test]=counters[test]+1
+print(counters)
+total=0
+for occ in counters:
+    total+=counters[occ]
+print(total)
