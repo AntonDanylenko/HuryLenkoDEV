30c03e6a43d7626cf9e57f1ad07a007dc79e4a44
Anton Danylenko <adanylenko@stuy.edu>
2018-09-23 21:54:06 -0400

    table works now

--- 09_20_18_classwork/appOccupations.py
+++ 09_20_18_classwork/appOccupations.py
@@ -12,33 +12,64 @@ app = Flask(__name__)
 def home():
     return "Welcome"
 
-diction = {}
+dict = {}
+#initializes the dictionary
 
-def convertToDict(filename):
-    f = open(filename, 'r')
-    text = f.read().split("\n")
-    for x in range (1, len(text)- 2):
-        cat = text[x].split(',', 1)
-        title = cat[0]
-        percent = float(cat[-1])
-        diction[title] = percent
 
-def randomOcc():
-    randlist = []
-    for key in diction:
-        current = diction[key]
-        freq = int(current *10)
-        for i in range (freq):
-            randlist.append(key)
-    print (random.choice(randlist))
+def convertToDict(fileName):
+    file = open(fileName, "r")
+    #open the data file and reads the contents
 
+    flines = file.readlines()[1:]
+    #readlines reads the individual lines of the file starting from the second
+
+    for line in flines:
+        #print(line)
+        quote = False
+        #initializes whether or not the occupation is in quotes as false
+        occupation = ""
+        #initializes the occupation name
+        num = False
+        #initializes whether we are parsing through the percent as false
+        percent = ""
+        #initializes the percent string
+        for character in line:
+            if character == "\"":
+                quote = not quote
+                #if a char is a quote, either turn quote true to start the quote
+                #or turn it false if the quote just ended.
+            elif character == ",":
+                if quote:
+                    occupation += character
+                    #if the comma in inside a quote, add it as part of the
+                    #occupation name.
+                else:
+                    num = True
+                    #if comma is not inside quote, start recording the percent
+            else:
+                if num:
+                    percent += character
+                    #if char is not a quote or a comma, and we are
+                    #recording the percent, add the char to the percent string
+                else:
+                    occupation += character
+                    #if char is not quote, comma, or part of the percent number,
+                    #add char to the occupation string.
+        #print(occupation)
+        #print(percent)
+        percent = percent.replace("\n", "")
+        #newlines were added to the end of the percent string from the end
+        #of every line, so we must remove them.
+        dict[occupation]= (float)(percent)
+
+convertToDict("data/occupations.csv")
 
 @app.route('/occupations')
 def test():
     return render_template('template2.html',
                                title = "Occupations",
                                heading = "This file selects a random occupation from the table below and displays it at the top.",
-                               collection = diction)
+                               collection = dict)
 
 if __name__ == "__main__":
     app.debug = True
