f927cd1fc44611ae1542fb20fbc2641bfd809414
Anton Danylenko <adanylenko@stuy.edu>
2018-09-23 21:29:54 -0400

    table headings are displayed but content is not, probably an error with how I use the dictionary

--- 09_20_18_classwork/appOccupations.py
+++ 09_20_18_classwork/appOccupations.py
@@ -3,6 +3,8 @@
 #Classwork
 #2018-09-20
 
+import random
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -10,11 +12,33 @@ app = Flask(__name__)
 def home():
     return "Welcome"
 
+diction = {}
+
+def convertToDict(filename):
+    f = open(filename, 'r')
+    text = f.read().split("\n")
+    for x in range (1, len(text)- 2):
+        cat = text[x].split(',', 1)
+        title = cat[0]
+        percent = float(cat[-1])
+        diction[title] = percent
+
+def randomOcc():
+    randlist = []
+    for key in diction:
+        current = diction[key]
+        freq = int(current *10)
+        for i in range (freq):
+            randlist.append(key)
+    print (random.choice(randlist))
+
+
 @app.route('/occupations')
 def test():
     return render_template('template2.html',
                                title = "Occupations",
-                               heading = "This file selects a random occupation from the table below and displays it at the top.")
+                               heading = "This file selects a random occupation from the table below and displays it at the top.",
+                               collection = diction)
 
 if __name__ == "__main__":
     app.debug = True
