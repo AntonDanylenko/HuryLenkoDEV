8fcec24b79b35a0b75e5abd6d10816c5c69e6648
Anton Danylenko <adanylenko@stuy.edu>
2018-10-01 23:37:01 -0400

    tried implementing cookies, stuck on make_response

---
+++ 14_login/app.py
@@ -0,0 +1,61 @@
+#Box2 - Anton Danylenko, Simon Tsui
+#SoftDev1 pd8
+#K14 -- Do I know you?
+#2018-10-01
+
+from flask import Flask
+from flask import session
+from flask import request
+from flask import render_template
+from flask import url_for
+from flask import redirect
+
+login_info = {"addis": "ababa"}
+usr_input = {}
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    #name = 'temp'
+    #pas = 'temp2'
+    #if (request.cookies.get("usrname"))
+    print(request.cookies.get("usrname"))
+    print(request.cookies.get("pswrd"))
+    if ((request.cookies.get("usrname") == "addis") & (request.cookies.get("pswrd") == "ababa")):
+        return render_template("auth.html", a = usr_input["Username"])
+    else:
+        return render_template("login.html")
+
+
+@app.route("/auth", methods = ['GET'])
+def authenticate():
+    #print(url_for('authenticate'))
+    usr_input["Username"] = request.args["Username"]
+    usr_input["Password"] = request.args["Password"]
+    resp = make_response(redirect(url_for('start')))
+    resp.set_cookie('usrname', usr_input["Username"])
+    resp.set_cookie('pswrd', usr_input["Password"])
+    if (request.args['Username'] == 'addis' and request.args['Password'] == 'ababa'):
+        return redirect(url_for("start"))
+    else:
+        return redirect(url_for('display_login'))
+
+@app.route("/again")
+def display_login():
+    if(usr_input["Username"] != "addis" and usr_input["Password"] != "ababa"):
+        return render_template("failed.html", a  = "Username and Password ")
+    elif(usr_input["Username"] != "addis"):
+        return render_template("failed.html", a = "Username")
+    else:
+        return render_template("failed.html", a = "Password")
+
+#@app.route("/display_login")
+#def success():
+#    print(request.args)
+    # return render_template("auth.html", a = request.args['Username'])
+#    return render_template("auth.html", a = usr_input["Username"])
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
