ea2794a81f92e34ccab7ae58516019d8b899f746
Anton Danylenko <adanylenko@stuy.edu>
2018-11-14 20:34:12 -0500

    s

---
+++ 25_rest/app.py
@@ -0,0 +1,21 @@
+# Anton Danylenko
+# SoftDev1 pd8
+# K25 -- Getting More REST
+# 2018-11-14
+
+import urllib, json
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = urllib.request.urlopen("http://developer.epa.gov/api/index.php/api_repellent?api=repellent")
+    respRead = response.read()
+    data = json.loads(respRead)
+    #print(data)
+    return render_template('app.html', pic = data['url'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
